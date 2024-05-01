7ffeb2cf0a7bc8dac3a9ecbb92fff00c2edb58e7
vittyGalaxy <tiozzovittorio25@gmail.com>
2024-05-01 19:04:03 +0200

    insertion of ships

--- main.cpp
+++ main.cpp
@@ -17,25 +17,52 @@ int main() {
         }
     }
     for (int i = 0; i < 9; i++){
+        switch (i) {
+            case 0:
+                cout << "nave da 4" << endl;
+                lenght = 4;
+                break;
+            case 1:
+                cout << "nave da 3" << endl;
+                lenght = 3;
+                break;
+            case 2:
+                cout << "nave da 3" << endl;
+                break;
+            case 3:
+                cout << "nave da 3" << endl;
+                break;
+            case 4:
+                cout << "nave da 2" << endl;
+                lenght = 2;
+                break;
+            case 5:
+                cout << "nave da 2" << endl;
+                break;
+            case 6:
+                cout << "nave da 2" << endl;
+                break;
+            case 7:
+                cout << "nave da 1" << endl;
+                lenght = 1;
+                break;
+            case 8:
+                cout << "nave da 1" << endl;
+                break;
+        }
         cout << "Inserisci x: ";
         cin >> x;
         cout << "Inserisci y: ";
         cin >> y;
-        // FIXME: posso inserire 9 navi a caso, sbagliato
-        // io farei un vettore di 9 elementi e imponi l'utente a mettere la nave giusta e dove posizionarla, non quanto lunga
-        
-        // while(verify == false){
-        //     cout << "Inserisci la lunghezza da 1 a 4: ";
-        //     cin >> lenght;
-        //     if ((lenght >= 1) && (lenght <= 4)){
-        //         verify = true;
-        //     }
-        // }
-        verify = false;
         cout << "Inserisci in che direzione '0' sotto, '1' a sinistra, '2' sopra, '3' a destra: ";
         cin >> direction;
         mode = (Mode)direction;
         insertBattleShip(battleField, x, y, lenght, mode);
+        verify = insertBattleShip(battleField, x, y, lenght, mode);
+        if (verify == false){
+            cout << "Rimetti la nave" << endl;
+            i--;
+        }
     }
     viewTable(battleField);
     return 0;
