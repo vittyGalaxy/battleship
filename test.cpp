70a271579eeba07af8769b053e4905a06a9d5b5e
AngeloDamante <angelo.damante16@gmail.com>
2024-06-08 03:51:03 +0200

    test: implemented tests for insert and remove

--- test.cpp
+++ test.cpp
@@ -1,5 +1,16 @@
-#include "include/BattleShip.h"
+#include "include/BattleField.h"
 #include <gtest/gtest.h>
+
+class BFSupervisor{
+    public:
+        BattleField oBF;
+
+        BFSupervisor(const BattleField& oBF): oBF(oBF){}
+
+        bool isThereAShip(int nXInit, int nYInit, int nXEnd, int nYEnd){
+            return oBF.isThereAShip(nXInit, nYInit, nXEnd, nYEnd);
+        }
+};
 
 TEST(createShip, correctGenericShip){
     BattleShip oBS_1(1,1,1,1);
@@ -30,6 +41,136 @@ TEST(createShip, wrongShip){
 
     BattleShip oBS_5(-1,-1,-1,-1);
     EXPECT_FALSE(oBS_5.isValid());
+}
+
+TEST(createShip, assignmentOperator){
+    BattleShip oBS_1(1,1,1,1);
+    EXPECT_EQ(oBS_1.getXInit(), 1);
+    
+    BattleShip oBS_3(4,2,6,2);
+    EXPECT_EQ(oBS_3.getXInit(), 4);
+
+    oBS_1 = oBS_3;
+    EXPECT_EQ(oBS_1.getXInit(), 4);
+    EXPECT_EQ(oBS_1.getYInit(), 2);
+    EXPECT_EQ(oBS_1.getXEnd(), 6);
+    EXPECT_EQ(oBS_1.getYEnd(), 2);
+}
+
+TEST(createShip, compareOperator){
+    BattleShip oBS_1(1,1,1,1);
+    BattleShip oBS_3(4,2,6,2);
+    EXPECT_EQ(oBS_1 == oBS_3, false);
+
+    oBS_1 = oBS_3;
+    EXPECT_EQ(oBS_1 == oBS_3, true);
+}
+
+TEST(insertBF, CorrectShip){
+    BattleShip oBS(1,1,1,3);
+    EXPECT_EQ(oBS.getLength(), 3);
+    EXPECT_EQ(oBS.isValid(), true);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    oBF.insertShip(oBS);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);    
+    EXPECT_EQ(BFSupervisor(oBF).isThereAShip(0,0,0,2), true);
+
+
+    BattleShip oBSMono(4,4,4,4);
+    EXPECT_EQ(oBSMono.getLength(), 1);
+    EXPECT_EQ(oBSMono.isValid(), true);
+    oBF.insertShip(oBSMono);
+    EXPECT_EQ(oBF.getNumberOfShips(), 2);  
+    EXPECT_EQ(BFSupervisor(oBF).isThereAShip(3,3,3,3), true);
+}
+
+TEST(insertBF, WrongShip){
+    BattleShip oBS(1,1,2,2);
+    EXPECT_EQ(oBS.isValid(), false);
+
+    BattleField oBF;
+    oBF.insertShip(oBS);
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+    EXPECT_EQ(oBF.isEmpty(), true);
+
+    EXPECT_EQ(BFSupervisor(oBF).isThereAShip(0,0,1,1), false);
+}
+
+TEST(insertBF, WrongDimensionShip){
+    BattleShip oBS(1,1,1,11);
+    EXPECT_EQ(oBS.getLength(), 11);
+    EXPECT_EQ(oBS.isValid(), true);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+    oBF.insertShip(oBS);
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+}
+
+TEST(insertBF, SameShips){
+    BattleShip oBS(1,1,1,3);
+    EXPECT_EQ(oBS.getLength(), 3);
+    EXPECT_EQ(oBS.isValid(), true);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+    oBF.insertShip(oBS);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+    oBF.insertShip(oBS);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+}
+
+TEST(insertBF, ShipWithCommonCells){
+    BattleShip oBS_1(1,1,1,3);
+    BattleShip oBS_2(3,3,1,1);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    oBF.insertShip(oBS_1);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+    oBF.insertShip(oBS_2);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+}
+
+TEST(removeBF, correctBS){
+    // BattleShip oBS_1(1,1,1,3);
+
+    // BattleField oBF;
+    // EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    // oBF.insertShip(oBS_1);
+    // EXPECT_EQ(oBF.getNumberOfShips(), 1);
+
+    // oBF.removeShip(oBS_1);
+    // EXPECT_EQ(oBF.getNumberOfShips(), 0);
+}
+
+TEST(removeBF, wrongBS){
+    BattleShip oBS_1(1,1,1,3);
+    BattleShip oBS_2(4,2,6,2);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    oBF.insertShip(oBS_1);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+
+    oBF.removeShip(oBS_2);
+    EXPECT_EQ(oBF.getNumberOfShips(), 1);
+}
+
+TEST(removeBF, emptyCase){
+    BattleShip oBS_1(1,1,1,3);
+
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    oBF.removeShip(oBS_1);
+    EXPECT_EQ(oBF.isEmpty(), true);
 }
 
 int main(){
