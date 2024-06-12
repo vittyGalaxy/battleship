b39354855f0871b125fa7f8d4fd1ad34b82fb7b9
AngeloDamante <angelo.damante16@gmail.com>
2024-06-12 22:33:06 +0200

    test: implemented tests for BattleField (insert, remove, save, load)

--- test.cpp
+++ test.cpp
@@ -12,6 +12,7 @@ class BFSupervisor{
         }
 };
 
+/*---------------------------------------------------------------------------*/
 TEST(createShip, correctGenericShip){
     BattleShip oBS_1(1,1,1,1);
     EXPECT_TRUE(oBS_1.isValid());
@@ -66,6 +67,7 @@ TEST(createShip, compareOperator){
     EXPECT_EQ(oBS_1 == oBS_3, true);
 }
 
+/*---------------------------------------------------------------------------*/
 TEST(insertBF, CorrectShip){
     BattleShip oBS(1,1,1,3);
     EXPECT_EQ(oBS.getLength(), 3);
@@ -73,7 +75,6 @@ TEST(insertBF, CorrectShip){
 
     BattleField oBF;
     EXPECT_EQ(oBF.getNumberOfShips(), 0);
-
 
     //oBF.show();
 
@@ -114,6 +115,11 @@ TEST(insertBF, WrongDimensionShip){
     EXPECT_EQ(oBF.getNumberOfShips(), 0);
     oBF.insertShip(oBS);
     EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    BattleShip oBSWrong(1,10,1,11);
+    bool bRes = oBF.insertShip(oBSWrong);
+    EXPECT_EQ(bRes, false);
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
 }
 
 TEST(insertBF, SameShips){
@@ -142,6 +148,7 @@ TEST(insertBF, ShipWithCommonCells){
     EXPECT_EQ(oBF.getNumberOfShips(), 1);
 }
 
+/*---------------------------------------------------------------------------*/
 TEST(removeBF, correctBS){
     BattleShip oBS_1(1,1,1,3);
 
@@ -184,6 +191,338 @@ TEST(removeBF, emptyCase){
 
     oBF.removeShip(oBS_1);
     EXPECT_EQ(oBF.isEmpty(), true);
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(saveLoadBF, ships){
+    // ships to be saved
+    BattleShip oBS_1(1,1,1,3);
+    BattleShip oBS_2(2,3,2,5);
+    BattleShip oBS_3(7,7,7,9);
+    BattleShip oBS_4(2,2,2,2);
+    BattleShip oBS_5(8,1,6,1);
+
+    // BF
+    BattleField oBF;
+    oBF.insertShip(oBS_1);
+    oBF.insertShip(oBS_2);
+    oBF.insertShip(oBS_3);
+
+    // check
+    EXPECT_EQ(oBF.getNumberOfShips(), 3);
+    oBF.save("bella.txt");
+    EXPECT_EQ(oBF.getNumberOfShips(), 3);
+
+    // New Battlefield
+    BattleField oBF_2;
+    EXPECT_EQ(oBF_2.getNumberOfShips(), 0);
+    bool bRes = oBF_2.load("bella.txt");
+    EXPECT_EQ(bRes, true);
+    EXPECT_EQ(oBF_2.getNumberOfShips(), 3);
+
+    // wrong filename
+    bRes = oBF_2.load("bell");
+    EXPECT_EQ(bRes, false);
+
+    // New Battlefield
+    BattleField oBF_3;
+    oBF_3.insertShip(oBS_4);
+    oBF_3.insertShip(oBS_5);
+    EXPECT_EQ(oBF_3.getNumberOfShips(), 2);
+    oBF_3.load("bella.txt");
+    EXPECT_EQ(oBF_3.getNumberOfShips(), 3);
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseOne){
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    for(int nI = 1; nI <= 10; nI++){
+        BattleShip oBS(nI, nI, nI, nI);
+        
+        oBF.insertShip(oBS);
+        EXPECT_EQ(oBF.getNumberOfShips(), 1);
+        EXPECT_EQ(oBF.getCell(nI-1, nI-1), BattleField::ship);
+
+        oBF.removeShip(oBS);
+        EXPECT_EQ(oBF.getNumberOfShips(), 0);
+        EXPECT_EQ(oBF.getCell(nI-1, nI-1), BattleField::water);
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseTwoVertical){
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    ---
+    |X|
+    |X|
+    ---
+    */
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI, nJ+1);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI+1, nJ, nI, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+
+                // EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseTwoHorizontal){
+    BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    |X X|
+    */
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI+1, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI+1, nJ, nI, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                // oBF.show();
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+}
+
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseThreeVertical){
+     BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    ---
+    |X|
+    |X|
+    |X|
+    ---
+    */
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI, nJ+2);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ+2, nI, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseThreeHorizontal){
+     BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    -------
+    |X X X|
+    -------
+    */
+
+       for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI+2, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseFourHorizontal){
+     BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    ---------
+    |X X X X|
+    ---------
+    */
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI+3, nJ);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+2), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI+2), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
+}
+
+/*---------------------------------------------------------------------------*/
+TEST(InsertRemove_IT, caseFourVertical){
+     BattleField oBF;
+    EXPECT_EQ(oBF.getNumberOfShips(), 0);
+
+    /*
+    ---
+    |X|
+    |X|
+    |X|
+    |X|
+    ---
+    */
+
+    for(int nI = 1; nI <= 10; nI++){
+        for(int nJ = 1; nJ <= 10; nJ++){
+            BattleShip oBS(nI, nJ, nI, nJ+3);
+
+            bool bRes = oBF.insertShip(oBS);
+            if(bRes){
+                EXPECT_EQ(oBF.getNumberOfShips(), 1);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::ship);
+                EXPECT_EQ(oBF.getCell(nJ+2, nI-1), BattleField::ship);
+
+                oBF.removeShip(oBS);
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+                EXPECT_EQ(oBF.getCell(nJ-1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ+1, nI-1), BattleField::water);
+                EXPECT_EQ(oBF.getCell(nJ+2, nI-1), BattleField::water);
+            }else{
+                EXPECT_EQ(oBF.getNumberOfShips(), 0);
+            }
+        }
+    }
 }
 
 int main(){
